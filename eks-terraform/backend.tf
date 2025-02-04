e273471622a9d4eb4c28a5aeb09caa5b33b0bea3
sekhar9346 <anakalasekhar004@gmail.com>
2025-02-04 10:59:45 +0530

    Update backend.tf

--- eks-terraform/backend.tf
+++ eks-terraform/backend.tf
@@ -9,7 +9,7 @@ terraform {
   backend "s3" {
     bucket = "ufygweid"
     key    = "eks/terraform.tfstate"
-    region = "us-east-1"
+    region = "eu-west-1"
   }
 
   required_version = ">= 1.6.3"
